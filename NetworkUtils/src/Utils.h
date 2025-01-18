ce23c0c96380aea68adfba0da7a6d2a022390693
Calvin Langat <calvokkt@gmail.com>
2025-01-18 19:47:45 +0000

    Added WINAPI thread func

--- NetworkUtils/src/Utils.h
+++ NetworkUtils/src/Utils.h
@@ -4,9 +4,13 @@
 #include <filesystem>
 #include <fstream>
 #include <string>
+#include <Windows.h>
 
 #pragma comment (lib, "Ws2_32.lib")
 
 #define DEFAULT_PORT "27015"
 #define DEFAULT_IP	 "127.0.0.1"
 #define DEFAULT_BUFFLEN 512
+
+// Thread function
+DWORD WINAPI workerthread(void* socketParam);
