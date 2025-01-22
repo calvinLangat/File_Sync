b7c7f62fa92c684055b33ab83b8d3be638140764
Calvin Langat <calvokkt@gmail.com>
2025-01-22 23:53:17 +0000

    null-terminate recv buffer and print msg

--- FileSync_Linux/src/Server/server.cpp
+++ FileSync_Linux/src/Server/server.cpp
@@ -4,8 +4,10 @@
 int main(int argc, char* argv[])
 {
     int opt = 1;
+    int conn_fd;
     struct sockaddr_in address;
-
+    const int bufferLen = 1024;
+    char buffer[bufferLen];
     // Create socket
     int listenSocket = socket(AF_INET, SOCK_STREAM, 0);
     if(listenSocket == -1)
@@ -39,5 +41,27 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    conn_fd = accept(listenSocket, NULL, NULL);
+    if(conn_fd == -1)
+    {
+        perror("Error accepting connection");
+        return -1;
+    }
+
+    size_t recv_bytes = recv(conn_fd, &buffer, bufferLen-1, 0); // Len-1 to allow null-termination
+    
+    if(recv_bytes > 0)
+    {
+        buffer[recv_bytes] = '\0';  // null-terminate
+        printf("message received: %s\n", buffer);
+    }
+    else if(recv_bytes == 0)
+    {
+        printf("Connection ended by client.\n");
+    }
+    else if(recv_bytes == -1)
+    {
+        perror("Error occured on recv");
+    }
     return 0;
 }
